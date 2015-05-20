719873562186564f8eb9bf6242e40e42f9bcf256
javi <javi@ubuntu.(none)>
2015-05-20 15:45:41 +0200

    improved temperature server

--- Temperature/TemperatureServer.py
+++ Temperature/TemperatureServer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys, traceback, Ice
 import Temp
 import time
@@ -20,28 +22,30 @@ if __name__ == "__main__":
   txt = Colors()
   try:
     ic = Ice.initialize(sys.argv)
-    adapter = ic.createObjectAdapterWithEndpoints("TemperatureAdapter", "default -p 10001")
+    adapter = ic.createObjectAdapterWithEndpoints("TemperatureAdapter", ic.getProperties().getProperty("TemperatureServer.Endpoints"))
     object = TemperatureI()
     adapter.add(object, ic.stringToIdentity("Temperature"))
     adapter.activate()
-
-    sys.stdout.write(txt.warning("Monitoring sensor...\t"))
-    sys.stdout.flush()
-    p = subprocess.Popen("sudo temper-poll", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-    out = p.stdout.readline()
-    if out.find("Found 0 devices") == -1:
-      print txt.bold(txt.green("Running --> " + out ))
-    else:
-      print txt.bold(txt.fail("Failed --> " + out ))
-      sys.exit()
-    while True:
-      p = subprocess.Popen("sudo temper-poll -qc", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-      T = p.stdout.readline()
-      time.sleep(1)
-    ic.waitForShutdown()
-
   except:
     status = 1
+    traceback.print_exc()
+
+  sys.stdout.write(txt.warning("Monitoring sensor...\t"))
+  sys.stdout.flush()
+  p = subprocess.Popen("sudo temper-poll", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+  out = p.stdout.readline()
+  if out.find("Found 0 devices") == -1:
+    print txt.bold(txt.green("Running --> " + out ))
+  else:
+    print txt.bold(txt.fail("Failed --> " + out ))
+    sys.exit()
+  while True:
+    p = subprocess.Popen("sudo temper-poll -qc", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    T = p.stdout.readline()
+    time.sleep(1)
+  ic.waitForShutdown()
+
+  
     
 
   if ic:
